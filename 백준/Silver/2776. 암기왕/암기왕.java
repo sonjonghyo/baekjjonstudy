4d61ef0a51e146bc2c45b01e115167ba251fe720
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-08-09 22:27:04 +0900

    [Silver IV] Title: 암기왕, Time: 1536 ms, Memory: 279784 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int tc = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= tc; i++) {
+			Set<Integer> memo1 = new HashSet<>();
+			int su1 = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int a = 0; a < su1; a++) {
+				int N = Integer.parseInt(st.nextToken());
+				memo1.add(N);
+			}
+			int su2 = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int a=0; a<su2; a++) {
+				if(memo1.contains(Integer.parseInt(st.nextToken()))) {
+					bw.write('1' + "\n");
+				}else {
+					bw.write('0' + "\n");
+				}
+			}	
+		}
+		bw.flush();
+		br.close();
+	}
+}

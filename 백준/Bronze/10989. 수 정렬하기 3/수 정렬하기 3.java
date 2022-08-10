0d321dd79ffdc5497bd6368f098fd6d1ffbc14fb
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-08-10 18:13:46 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1848 ms, Memory: 378020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int arr[]= new int[N];
+		int temp[] = new int[N];
+		int max = 0;
+		for(int i = 0; i<N; i++) {                        //N개의 입력을 받아옴.
+			arr[i] = Integer.parseInt(br.readLine());
+			if(max < arr[i]) {   //안에서 카운팅 정렬을 위해 바로 최대값을 구해준다.
+				max = arr[i];
+			}
+		}
+		int arr2[] = new int[max+1]; //최대값+1해준만큼 arr2배열을 생성해준다.
+		for(int i = 0; i<N; i++) {
+			arr2[arr[i]]++;  //arr[i]의 수를 보고 arr2[]를 ++ 즉 빈도수 체크를 해주는 부분이다.
+		}
+		for(int i = 1; i<arr2.length; i++) {
+			arr2[i] += arr2[i-1]; 
+		}
+		for(int i=N-1; i>=0; i--) {
+			temp[--arr2[arr[i]]] = arr[i];
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i =0; i<N; i++) {
+			sb.append(temp[i]);
+			sb.append("\n");
+
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+
+	}
+}

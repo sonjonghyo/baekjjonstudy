57742b26de502971491be5bc19e5ef1ece0be438
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-08-21 21:29:18 +0900

    [Bronze II] Title: 블랙잭, Time: 120 ms, Memory: 12956 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -5,24 +5,23 @@ public class Main {
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int arr[] = new int[N];
-		int sum = 0;
 		int max = 0;
-		for(int i =0; i<N; i++) {
+		int sum = 0;
+		for(int i = 0; i<N; i++) {
 			arr[i] = sc.nextInt();
 		}
-		for(int a = 0;a<N-2;a++) {
-			int fd = arr[a];
-			for(int b=1+a;b<N-1;b++) {
-				int sd = arr[b];
-				for(int c=1+b;c<N;c++) {
-					int td = arr[c];
-					sum = fd+sd+td;
-					if(sum==M) {
-						System.out.println(sum);
-						System.exit(0);
-					}
-					if(sum>max && sum<=M) {
+		loop:
+		for(int i = 0; i<N-2; i++) {
+			for(int j = i+1; j<N-1; j++) {
+				for(int z = j+1; z<N; z++) {
+					sum = arr[i] + arr[j] + arr[z];
+					if(sum == M) {
 						max = sum;
+						break loop;
+					}else {
+						if((sum > max) && (sum < M)) {
+							max = sum;
+						}
 					}
 				}
 			}

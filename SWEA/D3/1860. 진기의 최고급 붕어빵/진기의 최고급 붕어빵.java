4d31ed4609574cb99307217c364419045f126516
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-08-22 17:28:02 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 295 ms, Memory: 67,812 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
@@ -8,39 +8,29 @@ class Solution
       Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for(int tc = 1; tc<=T; tc++) {
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			int K = sc.nextInt();
-			int sum = 0;
+			int N = sc.nextInt(); //사람의 수를 입력.
+			int M = sc.nextInt(); //몇초마다 붕어빵을 만들지 결정.
+			int K = sc.nextInt(); //붕어빵을 몇개 만들지 결정.
+			int fishbread = 0; //붕어빵 개수
 			int arr[] = new int[N];
+			boolean is = false; //나중에 줄수있는지 아닌지를 구별해주기위해 boolean을 만들어준다.
 			for(int i = 0; i<N; i++) {
-				arr[i] = sc.nextInt();
+				arr[i] = sc.nextInt(); //몇초에 사람이 오는지 입력.
 			}
 			Arrays.sort(arr); //오름차순으로 정렬해 시간별로 세워둠.
-			int idx = 0; //arr에 있는 사람의 위치.
-			loop:
-			for(int i = 0; i<=arr[arr.length-1]; i++) { //중복 생각안했어.
-				if(M == 0) { //0초마다만들면 계속만드는거?.. 뭔가 말이안되긴하지만 0초면 계속 더해줌.
-					sum += K;
-				}else if(i != 0 && i%M==0) { //1초부터는 i%M==0이 될때 즉 정해진 시간에 도달했을때 붕어빵 추가.
-					sum += K;
-				}
-				while(arr[idx]==i) {//만약 arr[idx]가 i랑 같다면 while문 동작.				
-					if(sum == 0) {
-						System.out.println("#" + tc + " " + "Impossible"); //사람이왔는데 줄 붕어빵이없으므로 불가능
-						break loop; //for문 바깥으로 빼버림.
-					}
-					sum--; //sum이 0이아니므로 붕어빵을 하나 준다.
-					if(idx == arr.length-1) { //만약 idx가 마지막 즉 다줬다면
-						break; //브레이크를 걸어줌. 즉 while문만 빼줌.
-					}else { //아닐시엔
-					idx++; //다음 사람으로 변경.
-					}
-				}
-				if(i==arr[arr.length-1]) { //만약 시간이 다됐는데 중간에 끝나지 않았다면
-					System.out.println("#" + tc + " " + "Possible"); //가능함.
+			for(int i = 0; i<N; i++) {
+				fishbread = (arr[i]/M) * K - i; //붕어빵개수를 구하는데 (내가 받으러가는 시간/몇초마다 붕어빵이 나오는지) * 나오는 붕어빵수 - 앞사람이 가져간갯수.
+				if(fishbread <= 0) { //만약 붕어빵이 0이하라면 true로 만들어주어 for문을 끝내준다.(붕어빵을 가져갈수 없으므로.)
+					is = true;
+					break;
 				}
 			}
-		}
+			if(is) { //만약 true라면 붕어빵을 못가져가는사람이 있음.
+				System.out.println("#" + tc + " " + "Impossible"); //불가능함.
+			}else {
+				System.out.println("#" + tc + " " + "Possible"); //가능함
+			}
+        }
+        sc.close();
 	}
 }

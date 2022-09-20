8d94a82fbe8ec2ba9248c858f78ff181914f4719
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-09-20 23:58:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 76 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -3,31 +3,39 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
+	static int mini[];
+	static int temp[];
+	static int sum;
+	static int result[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int height[] = new int[9];
-		int sum = 0;
-		int a = 0;
-		int b = 0;
-		for (int i = 0; i < 9; i++) {
-			height[i] = Integer.parseInt(br.readLine());
-			sum += height[i];
+		mini = new int[9]; //아홉명 주어주므로 9로 만들고
+		temp = new int[7]; //임시적으로 저장될 7명에 대한 배열.
+		result = new int[7]; //결과를 저장해줄 7명에 대한 배열
+		for(int i = 0; i<9; i++) {
+			mini[i] = Integer.parseInt(br.readLine());
 		}
-		Arrays.sort(height);
-		for (int i = 0; i < 8; i++) {
-			for (int j = i + 1; j < 9; j++) {
-				if (sum - height[i] - height[j] == 100) {
-					a = i;
-					b = j;
-					break;
-				}
+		comb(0,0);
+		Arrays.sort(result);
+		for(int i = 0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	public static void comb(int idx, int sidx) {
+		if(sidx==temp.length) {
+			sum = 0;
+			for(int i = 0; i<sidx; i++) {
+				sum += temp[i];
 			}
+			if(sum==100) {
+				result=temp.clone();
+			}	
+		}else if(idx >= mini.length) {
+			return;			
+		}else {
+			temp[sidx] = mini[idx];
+			comb(idx+1, sidx+1);
+			comb(idx+1, sidx);
 		}
-		for (int k = 0; k < 9; k++) {
-			if(k == a || k == b)
-				continue;
-			System.out.println(height[k]);
-		}
-		br.close();
 	}
 }

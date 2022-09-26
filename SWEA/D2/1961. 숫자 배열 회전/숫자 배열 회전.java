6bc5c553a9aab6ce02b51ef544926fc9bb7a0f0e
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-09-26 19:01:56 +0900

    [D2] Title: 숫자 배열 회전, Time: 141 ms, Memory: 21,444 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,44 +1,40 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-				Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int i = 1; i<=T; i++) {
-			int N = sc.nextInt();
-			String [] arr[] = new String[N][N];
-			String [] arrsum[] = new String[N][N];
-			for(int j = 0; j<arr.length; j++) {
-				for(int z = 0; z<arr.length; z++) {
-					arr[j][z] = sc.next();
-					arrsum[j][z] = "";
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt(); //test케이스 반복횟수 설정.
+		for(int tc = 1; tc<=T; tc++) { //1부터 출력하기 위해 1부터 <=T까지.
+			int N = sc.nextInt(); //2차원 배열의 크기.
+			int arr[][] = new int[N][N];
+			String result[][] = new String[N][N];
+			for(int i = 0; i<N; i++) {
+				for(int j = 0; j<N; j++) {
+					arr[i][j] = sc.nextInt();
+					result[i][j] = ""; //전부 빈칸으로 만들어줌.
 				}
 			}
-		    for(int j = 0; j<arr.length; j++) {
-		        for(int z = 0; z<3; z++) {
-				    for(int h = 0; h<arr.length; h++) {
-				    	if(z==0) {
-				    		arrsum[j][z] += arr[arr.length-1-h][j];	 //h가 0일때는 이거 됨.
-				    	} else if(z%2==1) {
-				    		arrsum[j][z] += arr[arr.length-1-j][arr.length-1-h]; //h가 1일때
-				    	} else if(z%2==0) {
-				    		arrsum[j][z] += arr[h][arr.length-1-j]; //h가 2일때
-				    		
-				    	}
-				    }
+			//이후로는 규칙을 찾아 회전시킨다.
+			for(int i = 0; i<N; i++) {
+				for(int j = 0; j<3; j++) {
+					for(int z= 0; z<N; z++) {
+						if(j==0) {
+							result[i][j] += String.valueOf(arr[N-1-z][i]);						
+						}else if(j%2==1) {
+							result[i][j] += String.valueOf(arr[N-1-i][N-1-z]);
+						}else if(j%2==0) {
+							result[i][j] += String.valueOf(arr[z][N-1-i]); 
+						}	
+					}
 				}
-		
-	        }
-		    System.out.println("#"+i);
-		    for(int a = 0; a<arrsum.length; a++) {
-		    	for(int b= 0; b<arrsum.length; b++) {
-		    		 System.out.print(arrsum[a][b] + " ");
-		    	}
-		    	System.out.println();
-		    }
+			}
+			System.out.println("#" + tc);
+			for(int i = 0; i<N; i++) {
+				for(int j = 0; j<N; j++) {
+					System.out.print(result[i][j]+" ");
+				}
+				System.out.println();
+			}
 		}
 	}
 }

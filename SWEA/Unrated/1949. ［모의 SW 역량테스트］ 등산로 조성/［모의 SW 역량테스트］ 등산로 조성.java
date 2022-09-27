9a83a70c7ea60b432fcecb520c360c55c37512aa
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-09-28 02:01:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 201 ms, Memory: 58,748 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -46,9 +46,9 @@ public class Solution {
 			}
 			for(int i = 0; i<N; i++) {
 				for(int j = 0; j<N; j++) {
-					if(arr[i][j]==max) {
-						visited = new boolean[N][N];
-						BFS(i,j,max);			
+					if(arr[i][j]==max) { //최대값과 현재 위치가 같다면
+						visited = new boolean[N][N]; //그때 배열생성후(최대 봉우리마다 해줘야하므로.)
+						BFS(i,j,max); //BFS 실행.
 					}
 				}
 			}
@@ -65,7 +65,7 @@ public class Solution {
 		
 		while(!q.isEmpty()) {
 			Node n = q.poll();
-			boolean[][] realvisit = new boolean[N][N];	
+			boolean[][] realvisit = new boolean[N][N];	//깊은복사 사용
 			realvisit = copy(n.visit);
 			realvisit[n.r][n.c] = true;
 			
@@ -78,23 +78,23 @@ public class Solution {
 					continue;
 				if(n.visit[nr][nc])
 					continue;
-				if(arr[nr][nc] < n.high) {
+				if(arr[nr][nc] < n.high) { //만약 최고높이보다 현재높이가 더 낮다면
 					q.add(new Node(nr,nc, n.cnt+1,arr[nr][nc],n.cut,realvisit)); //nr,nc의 위치를 넣어주고 한칸이동했으므로 n.cnt+1을해주고 봉우리의 최대값은
 					//arr[nr][nc]의 값이고 마지막은 잘랐는지 안잘랐는지 확인.
-				} else if(n.cut == 1) {
-					for(int i =1; i<=K; i++) {
-						if(arr[nr][nc]-i < n.high ) {
+				} else if(n.cut == 1) { //만약 아직 자를수있는 기회가있고 최고높이보다 현재높이가 더 높다면
+					for(int i =1; i<=K; i++) { //하나씩 잘라서 
+						if(arr[nr][nc]-i < n.high ) { //깊이가 낮아질때까지 넣어주기.
 							q.add(new Node(nr,nc,n.cnt+1,arr[nr][nc]-i,0,realvisit));
 					}
 				}
 			}
 		}
-			if(rmax < n.cnt)
-				rmax = n.cnt;
+			if(rmax < n.cnt) //cnt가 rmax보다 크다면
+				rmax = n.cnt; //저장.
 		}
 	}
 	
-	public static boolean[][] copy(boolean[][] co) {
+	public static boolean[][] copy(boolean[][] co) { //깊은 복사사용
 		boolean[][] check = new boolean[N][N];
 		for(int i = 0; i<N; i++) {
 			System.arraycopy(co[i], 0, check[i] , 0, check[i].length);

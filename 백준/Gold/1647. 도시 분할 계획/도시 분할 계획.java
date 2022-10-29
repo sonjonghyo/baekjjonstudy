f829f6c364ab905a21e7dff899b7690ca6823862
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2022-10-29 16:27:45 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2336 ms, Memory: 348156 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,64 +1,67 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-//크루스칼 방식 사용.
-public class Main { 
-	static int p[];
+
+public class Main {
+	static class Edge implements Comparable<Edge> {
+		int st, ed, cost;
+		
+		public Edge(int st, int ed, int cost) {
+			this.st = st;
+			this.ed = ed;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.cost - o.cost;
+		}
+	}
+	static int max_w = 0;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken()); //정점의 개수
-		int M = Integer.parseInt(st.nextToken()); //간선의 개수 
+		int M = Integer.parseInt(st.nextToken()); //간선의 개수
 		
-		int edges[][] = new int[M][3];
+		List<Edge>[] adjList = new ArrayList[N+1]; //인접 리스트 생성.
+		for(int i = 0; i<N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
 		for(int i = 0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			edges[i][0] = Integer.parseInt(st.nextToken()); //시작정점
-			edges[i][1] = Integer.parseInt(st.nextToken()); //도착정점
-			edges[i][2] = Integer.parseInt(st.nextToken()); //가중치
-		}
-		
-		Arrays.sort(edges, new Comparator<int[]>() { //가중치 오름차순으로 정렬.
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				return o1[2] - o2[2];
-			}
+			int s = Integer.parseInt(st.nextToken())-1; //시작지점
+			int e = Integer.parseInt(st.nextToken())-1; //도착지점
+			int w = Integer.parseInt(st.nextToken()); //가중치
 			
-		});
-		
-		p = new int[N+1]; //대표를 저장해줄 친구.
-		for(int i = 1; i<=N; i++) {
-			p[i] = i; //나 자신을 대표로 먼저 초기화.
-		}
-		int pick = 0;
+			adjList[s].add(new Edge(s,e,w)); //시작지점에서 끝지점으로 가는 것 리스트에 추가.
+			adjList[e].add(new Edge(e,s,w)); //끝지점에서 시작지점으로 가는 것 리스트에 추가.
+		} //인접행렬 완성
+		boolean visited[] =  new boolean[N+1]; //방문처리용.
+		PriorityQueue<Edge> pq = new PriorityQueue<>(); //우선순위 큐 적용.
+		//가중치가 낮은것 부터 시작하기위해 우선순위 큐 사용함.
+		visited[0] = true;
+		pq.addAll(adjList[0]);
+		//1이 시작지점, 끝지점에 있는 것을 전부 담아줘.
+		int pick = 1;
 		int dis = 0;
-		for(int i = 0; i<M; i++) {
-			int px = findSet(edges[i][0]);
-			int py = findSet(edges[i][1]);
+		while(pick < N) {
+			Edge edge = pq.poll();
+			if(visited[edge.ed]) continue; //이미 도착했던곳이면 다음.
 			
-			if(px != py) {
-				union(px,py);
-				dis += edges[i][2];
-				pick++;
-			}
-			if(pick==N-2) break;
+			visited[edge.ed] = true; //현재 시작->도착은 방문되었으니 true로 변경.
+			max_w = Math.max(max_w, edge.cost);
+			dis += edge.cost; //가중치를 거리에 추가시키고
+			pq.addAll(adjList[edge.ed]); //도착지점에 해당하는 부분 모두 리스트에 담아주기.
+			pick++;
 		}
-		System.out.println(dis);
-		
+		System.out.println(dis-max_w);
 	}
-	private static void union(int x, int y) {
-		p[y] = x;
-		
-	}
-	private static int findSet(int i) {
-		if(i != p[i])
-			p[i] = findSet(p[i]);
-		return p[i];
-	}
-	
 }

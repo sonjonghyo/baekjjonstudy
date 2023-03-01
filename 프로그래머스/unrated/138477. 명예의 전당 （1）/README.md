45e2698fcc9f75dac4ccbd268a577429ce17aade
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-03-01 14:27:43 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.05 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [unrated] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=java) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.34 ms
+메모리: 70.2 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,17 +1,46 @@
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        List<Integer> rank = new ArrayList<>();
 		int answer[] = new int[score.length];
 		for(int i = 0; i < score.length; i++) {
-			pq.add(score[i]);
+			if(rank.size() == 0) {
+				rank.add(score[i]);
+			}else {
+				if(rank.get(0) < score[i]) {
+					rank.add(0, score[i]);
+				}else {
+					if(rank.size() >= k) {
+						for(int j = 0; j < k; j++) {
+							if(rank.get(j) <= score[i]) {
+								rank.add(j, score[i]);
+								break;
+							}
+						}						
+					}else {
+						for(int j = 0; j < rank.size(); j++) {
+							if(rank.get(j) <= score[i]) {
+								rank.add(j, score[i]);
+								break;
+							}
+							if(j == rank.size()-1) {
+								rank.add(rank.size(), score[i]);
+								break;
+							}
+						}
+					}
+				}
+			}
+			if(rank.size() < k) {
+				answer[i] = rank.get(i);
+			}else {
+				answer[i] = rank.get(k-1);
+			}
 			
-			if(pq.size() > k)
-				pq.poll();
-			
-			answer[i] = pq.peek();
 		}
+
         return answer;
     }
 }

69a9bd6aeebcfda4f3dc3ea3609aaabd01b7123f
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-05-22 19:13:01 +0900

    [level 2] Title: 오픈채팅방, Time: 79.47 ms, Memory: 151 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public List solution(String[] record) {
+        List<String> answer = new ArrayList<>();
+		HashMap<String, String> nickUID = new HashMap<>();
+		String splitRecord[][] = new String[record.length][3];
+		for(int i = 0; i < record.length; i++) {
+			splitRecord[i] = record[i].split(" ");
+		}
+		for(int i = 0; i < record.length; i++) {
+			if(!splitRecord[i][0].equals("Leave")) {
+				nickUID.put(splitRecord[i][1], splitRecord[i][2]);				
+			}
+		}
+		for(int i = 0; i < record.length; i++) {
+			if(splitRecord[i][0].equals("Enter")) {
+				answer.add(nickUID.get(splitRecord[i][1]) + "님이 들어왔습니다.");
+			}else if(splitRecord[i][0].equals("Leave")) {
+				answer.add(nickUID.get(splitRecord[i][1]) + "님이 나갔습니다.");
+			}
+		}
+        return answer;
+    }
+}

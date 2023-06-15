5d78b3178560ba3e1827ba866e3d008d5803d0f6
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-06-15 16:50:21 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,30 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int cnt = 1;
-        StringBuilder sb = new StringBuilder();
-        while(true) {
-            if(Math.pow(3,cnt) >= n) {
-                break;
-            }else {
-                cnt++;
-            }
+         StringBuilder sb = new StringBuilder();
+        while(n != 0) {
+            sb.append((n%3));
+            n /= 3;
         }
-        if(Math.pow(3, cnt) == n) {
-        	sb.append(1);
-        	n = 0;
-        }
-        cnt--;
-        while(cnt != -1) {
-            int num = (int)Math.pow(3,cnt--);
-            int a = n / num;
-            n = n - (num * a);
-            sb.append(a);
-        }
-        String three = sb.toString();
-        for(int i = 0 ; i < three.length(); i++) {
-        	int a = three.charAt(i)-48;
-        	answer += (int)Math.pow(3, i) * a;
+        String three = sb.reverse().toString();
+        for(int i = 0; i < three.length(); i++) {
+        	answer += Math.pow(3, i) * (three.charAt(i) - '0');
         }
         return answer;
     }

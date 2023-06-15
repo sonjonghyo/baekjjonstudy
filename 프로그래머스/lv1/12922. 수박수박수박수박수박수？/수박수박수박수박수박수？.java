01777486f61f9c983e3563f76349ad7e60356a8a
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-06-15 13:17:50 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public StringBuilder solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                answer.append("수");
+            }else {
+                answer.append("박");
+            }
+        }
+        return answer;
+    }
+}

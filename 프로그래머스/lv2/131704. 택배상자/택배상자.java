da3eec38b7cfb4fff85add29ae063689190d0cd1
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-07-04 09:38:18 +0900

    [level 2] Title: 택배상자, Time: 71.31 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -7,18 +7,6 @@ class Solution {
         int cnt = order[turn];
         Stack<Integer> stack = new Stack<>();
         for(int i = 0; i < order.length; i++) {
-            while(!stack.isEmpty()) {
-                if(stack.peek() == cnt) {
-                    stack.pop();
-                    answer++;
-                    turn++;
-                    if(turn < order.length) {
-                        cnt = order[turn];
-                    }    
-                }else {
-                    break;
-                }
-            }
             if(cnt == i+1) {
                 turn++;
                 answer++;
@@ -28,18 +16,18 @@ class Solution {
             }else {
                 stack.add(i+1);
             }
-        }
-        while(!stack.isEmpty()) {
-        	if(stack.peek() == cnt) {
-        		stack.pop();
-        		answer++;
-        		turn++;
-                if(turn < order.length) {
-                       cnt = order[turn];
-                   }
-        	}else {
-        		break;
-        	}
+            while(!stack.isEmpty()) {
+                if(stack.peek() == cnt) {
+                    stack.pop();
+                    answer++;
+                    turn++;
+                 if(turn < order.length) {
+                        cnt = order[turn];
+                    }       
+                }else {
+                break;
+                }
+            }
         }
         return answer;
     }

7bb5366cea76e983c164ba2f5f6a5327e5e77363
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-07-25 17:46:15 +0900

    [level 2] Title: 구명보트, Time: 8.57 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int idx = 0;
+        Arrays.sort(people);
+        for(int i = people.length-1; i >= idx; i--) {
+            if(people[i] + people[idx] <= limit) {
+                idx++;
+                answer++;
+            }else {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}

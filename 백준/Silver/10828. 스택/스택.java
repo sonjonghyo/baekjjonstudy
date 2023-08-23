9159c5803b6dd5c65eaffc98996d357b654cabfc
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-08-23 21:41:00 +0900

    [Silver IV] Title: 스택, Time: 148 ms, Memory: 16888 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,85 +1,53 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int top = -1;
-	static int arr[];
-	static int size = 0;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		Stack<Integer> stack = new Stack<>(); //스택을 넣어주자.
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		arr = new int[10000];
-		for(int i =0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			String behavior = st.nextToken();
-			switch(behavior) {
-			case "push" :
-				int num = Integer.parseInt(st.nextToken());
-				push(num);
-				break;
-			case "pop" :
-				pop();
-				if(i!=N-1)
-					enter();
-				break;
-			case "empty" :
-				empty();
-				if(i!=N-1)
-					enter();
-				break;
-			case "size" :
-				size();
-				if(i!=N-1)
-					enter();
-				break;
-			case "top" :
-				top();
-				if(i!=N-1)
-					enter();
-				break;
+		int num = 0;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()); //명령어를 넣어줄 부분입니다.
+			String s = st.nextToken();
+			if(s.equals("push")) { //만약 넣은 명령어가 push라면
+				num = Integer.parseInt(st.nextToken()); //어떤 수인지 알아야하므로 num 변수에 저장해줍니다.
 			}
+			switch(s) { //switch문을 사용해 어떤 명령어 인지 확인하자.
+			case "push": //정수 num을 stack에 넣어주는 연산
+				stack.add(num);
+				break;
+			case "pop" : //만위의 정수를 빼고 수를 출력. 스택에 정수가 없으면 -1 출력.
+				if(stack.isEmpty()) { //만약 stack이 비어있다면?
+					sb.append(-1); //-1을 넣어주기
+				}else { //그와 반대 상황이라면?
+					sb.append(stack.pop()); //맨위 수를 넣어주기.
+				}
+				break;
+			case "size" : //현재 stack의 size를 출력하기
+				sb.append(stack.size());
+				break;
+			case "empty" : //stack이 비어있는지 확인
+				if(stack.isEmpty()) { //비어있다면 1
+					sb.append(1);
+				}else { //아니면 0
+					sb.append(0);
+				}
+				break;
+			case "top" : //stack의 맨위 정수 출력
+				if(stack.isEmpty()) { //비어있다면?
+					sb.append(-1); //-1 출력
+				}else { //스택안에 정수가 있다면
+					sb.append(stack.peek()); //맨위 정수 출력
+				}
+				break;
+				
+			}
+			if(i != N-1 && !s.equals("push")) //마지막을 제외하고는 개행문자 추가
+				sb.append("\n");
 		}
 		System.out.println(sb);
-	}
-	
-	static void push(int num) {
-		arr[++top] = num;
-		size++;
-	}
-	
-	static void pop() {
-		if(size==0) {
-			sb.append(-1);
-		}else {
-			int pnum = arr[top--];
-			sb.append(pnum);
-			size--;
-		}
-	}
-	
-	static void empty() {
-		if(size==0) {
-			sb.append(1);
-		}else {
-			sb.append(0);
-		}
-	}
-	
-	static void size() {
-		sb.append(size);
-	}
-	
-	static void top() {
-		if(size==0) {
-			sb.append(-1);
-		}else {
-			sb.append(arr[top]);			
-		}
-	}
-	static void enter() {
-		sb.append("\n");
 	}
 }

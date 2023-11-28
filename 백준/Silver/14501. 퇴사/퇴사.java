84c0dbc8de283af4d212891770747e13e276d2f0
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-11-28 21:23:02 +0900

    [Silver III] Title: 퇴사, Time: 76 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,53 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int arr[][];
-	static int N;
-	static int max = Integer.MIN_VALUE;
-	static boolean visited[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1][2];
-		visited = new boolean[N+1];
-		for(int i = 1; i<=N; i++) {
+		//남은 일
+		int N = Integer.parseInt(br.readLine());
+		//걸리는 기간과 받을 수 있는 금액.
+		int TP[][] = new int[N][2];
+		int dp[] = new int[N+1];
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			//기간.
+			TP[i][0] = Integer.parseInt(st.nextToken());
+			//금액.
+			TP[i][1] = Integer.parseInt(st.nextToken());			
 		}
-		int sum = 0;
-		perms(1,sum);
-		System.out.println(max);
-		
-	}
-	public static void perms(int idx, int sum) {
-		if(idx>N+1) //idx가 N보다 클시엔 수행이 안되므로
-			return; //return시켜준다.
-		if(idx<=N+1) { //N보다 작을시엔
-			if(max < sum) //현재까지 더해준 sum이 max보다 작을시에
-				max = sum; //max에 sum을 더해줌.
-		}
-		
-		for(int i = idx; i<=N; i++) {
-			if(!visited[i]) { //아직 방문을 안했어야 들어감.
-				int t = arr[i][0] + i; //현재 가능한시점에서 얼마나 시간이 걸리는지 계산.
-				if(t>N+1) //만약 시간이 넘어가는건 수행할수없으므로 return시켜준다.
-					continue;
-				for(int r = i; r<t; r++) { //수행한 시간은 전부 true로 바꿔줌.
-					visited[r] = true;
-				}
-				sum += arr[i][1]; //처음 시작한 부분을 sum에 추가시켜준다.
-				perms(i+1, sum); //다음 idx를 다시 재시행.
-				for(int r = i; r<t; r++) { //방문한부분을 다시 false로 만들어줌.
-					visited[r] = false;
-				}
-				sum -= arr[i][1];
-				perms(i+1, sum);
+		for(int i = N-1; i >= 0; i--) {
+			//일할 수 있는 기간 내라면?
+			if(i + TP[i][0] <= N) {
+				//비교를 해주어야 함.
+				dp[i] = Math.max(dp[i + TP[i][0]] + TP[i][1], dp[i+1]);
+			//일을 할 수 없는 기간이라면?
+			}else {
+				//그냥 이전에 있는 시간을 넣어주자.
+				dp[i] = dp[i+1];
 			}
 		}
-			
+		//dp의 맨 처음에 있는 값이 가장 최대 수익.
+		System.out.println(dp[0]);
 	}
 }

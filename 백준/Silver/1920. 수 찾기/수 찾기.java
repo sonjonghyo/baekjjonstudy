956296f8ce05b4438f8ee3f3da25e8b5e98c00b5
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-12-13 16:23:10 +0900

    [Silver IV] Title: 수 찾기, Time: 2360 ms, Memory: 100824 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,35 +1,56 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		Set<Integer> memo1 = new HashSet<>();
-		int su1 = Integer.parseInt(br.readLine());
+		//배열의 크기 N
+		int N = Integer.parseInt(br.readLine());
+		//정수들을 담을 배열 num 선언
+		int num[] = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int a = 0; a < su1; a++) {
-			int N = Integer.parseInt(st.nextToken());
-			memo1.add(N);
+		for(int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		int su2 = Integer.parseInt(br.readLine());
+		//이분탐색을 위해 먼저 num을 정렬해 준다.
+		Arrays.sort(num);
+		
+		int M = Integer.parseInt(br.readLine());
+		int check[] = new int[M];
 		st = new StringTokenizer(br.readLine());
-		for (int a = 0; a < su2; a++) {
-			if (memo1.contains(Integer.parseInt(st.nextToken()))) {
-				bw.write('1' + "\n");
-			} else {
-				bw.write('0' + "\n");
+		for(int i = 0; i < M; i++) {
+			check[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i = 0; i < M; i++) {
+			//왼쪽은 0번째
+			int left = 0;
+			//오른쪽은 배열의 가장 마지막 번째
+			int right = N-1;
+			//flag를 사용해 true로 바뀌면 찾은거로 1 출력 아니면 0 출력
+			boolean flag = false;
+			//왼쪽보다 오른쪽이 더 커지면 끝.
+			while(left <= right) {
+				//먼저 중간 지점을 구한다.
+				int mid = (left + right) / 2;
+				//만약 해당 위치의 값이 검사해야하는 값보다 크다면?
+				if(num[mid] > check[i]) {
+					right = mid - 1;
+				//반대로 위치의 값보다 작다면?
+				}else if(num[mid] < check[i]) {
+					left = mid + 1;
+				//두개의 값이 같다면?
+				}else {
+					//flag를 true로 만들어주고 끝낸다.
+					flag = true;
+					break;
+				}
+			}
+			if(flag) {
+				System.out.println(1);
+			}else {
+				System.out.println(0);
 			}
 		}
-		bw.flush();
-		br.close();
 	}
 }

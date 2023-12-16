d2d42fe90ef51407454a71132e5b50a3f4471343
sonjonghyo <110041044+sonjonghyo@users.noreply.github.com>
2023-12-17 02:05:02 +0900

    [Silver II] Title: 나무 자르기, Time: 472 ms, Memory: 168132 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,39 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N,M;
-	static int tree[];
-	static int max = 0;
-	public static void main(String[] args) throws Exception{
+	static int max  = 0;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		int result = 0;
-		tree = new int[N];
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int tree[] = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
+		for(int i = 0; i < N; i++) {
 			tree[i] = Integer.parseInt(st.nextToken());
-			if(tree[i] > max)
+			//최댓값 설정
+			if(tree[i] > max) {
 				max = tree[i];
+			}
 		}
-		result = binarysearch();
-		System.out.println(result);
+		System.out.println(searchMax(N, M, tree));
 	}
-	private static int binarysearch() {
+	private static int searchMax(int n, int m, int[] tree) {
 		int left = 0;
 		int right = max;
 		while(left <= right) {
-			int mid = (left+right)/2;
+			//중간값 설정.
+			int mid = (left + right) / 2;
 			long sum = 0;
-			for(int i = 0; i<N; i++) {
-				if(tree[i] > mid)
-					sum += tree[i] - mid;
+			for(int i = 0; i < n; i++) {
+				//만약 나무의 길이가 mid값보다 크면 자를수 있는게 생김.
+				if(tree[i] > mid) {
+					sum +=  tree[i] - mid;
+				}
 			}
-			if(M <= sum) {
+			if(sum >= m) {
 				left = mid + 1;
 			}else {
 				right = mid - 1;
